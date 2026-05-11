c0c1f4bbce0aa5336fefcb76be71f0ef95efa423
kioi23 <kioijgithinji23@gmail.com>
2026-05-11 18:48:22 +0300

    refactor ToyCard component to utilize props for toy data and streamline event handling

--- src/components/ToyCard.jsx
+++ src/components/ToyCard.jsx
@@ -1,56 +1,33 @@
-import React from "react";
+function ToyCard({ toy, onDeleteToy, onUpdateToy }) {
+  function handleDelete() {
+    fetch(`http://localhost:3001/toys/${toy.id}`, {
+      method: "DELETE"
+    }).then(() => onDeleteToy(toy.id));
+  }
 
-function ToyCard() {
+  function handleLike() {
+    const updatedLikes = toy.likes + 1;
+
+    fetch(`http://localhost:3001/toys/${toy.id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ likes: updatedLikes })
+    })
+      .then((res) => res.json())
+      .then((updatedToy) => onUpdateToy(updatedToy));
+  }
+
   return (
-    <div className="card" data-testid="toy-card">
-      <h2>{"" /* Toy's Name */}</h2>
-      <img
-        src={"" /* Toy's Image */}
-        alt={"" /* Toy's Name */}
-        className="toy-avatar"
-      />
-      <p>{"" /* Toy's Likes */} Likes </p>
-      <button className="like-btn">Like {"<3"}</button>
-      <button className="del-btn">Donate to GoodWill</button>
+    <div>
+      <h2>{toy.name}</h2>
+      <img src={toy.image} alt={toy.name} width="200" />
+      <p>{toy.likes} Likes</p>
+      <button onClick={handleLike}>Like ❤️</button>
+      <button onClick={handleDelete}>Donate</button>
     </div>
   );
 }
-function handleDelete() {
-  fetch(`http://localhost:3001/toys/${toy.id}`, {
-    method: "DELETE"
-  })
-  .then(() => onDeleteToy(toy.id));
-}
-<button onClick={handleDelete}>Donate</button>
-
-function handleDeleteToy(id) {
-  const updatedToys = toys.filter((toy) => toy.id !== id);
-  setToys(updatedToys);
-}
-<ToyCard
-  key={toy.id}
-  toy={toy}
-  onDeleteToy={handleDeleteToy}
-/>
-
-function handleLike() {
-  const updatedLikes = toy.likes + 1;
-
-  fetch(`http://localhost:3001/toys/${toy.id}`, {
-    method: "PATCH",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify({ likes: updatedLikes })
-  })
-    .then((res) => res.json())
-    .then((updatedToy) => {
-      onUpdateToy(updatedToy);
-    });
-}
-<button onClick={handleLike}>
-  {toy.likes} Likes
-</button>
-
 
 export default ToyCard;
