7d9b6fc94d8939d0592b33418da3a6b3d36db3b8
kioi23 <kioijgithinji23@gmail.com>
2026-05-11 18:46:31 +0300

    refactor ToyForm component to use props and streamline form submission

--- src/components/ToyForm.jsx
+++ src/components/ToyForm.jsx
@@ -1,68 +1,46 @@
-import React from "react";
+import { useState } from "react";
 
-function ToyForm() {
+function ToyForm({ onAddToy }) {
   const [name, setName] = useState("");
   const [image, setImage] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    const newToy = {
+      name,
+      image,
+      likes: 0
+    };
+
+    fetch("http://localhost:3001/toys", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(newToy)
+    })
+      .then((res) => res.json())
+      .then((createdToy) => onAddToy(createdToy));
+  }
+
   return (
-    <div className="container">
-      <form className="add-toy-form">
-        <h3>Create a toy!</h3>
-        <input
-          type="text"
-          name="name"
-          placeholder="Enter a toy's name..."
-          className="input-text"
-        />
-        <br />
-        <input
-          type="text"
-          name="image"
-          placeholder="Enter a toy's image URL..."
-          className="input-text"
-        />
-        <br />
-        <input
-          type="submit"
-          name="submit"
-          value="Create New Toy"
-          className="submit"
-        />
-        <input
-  type="text"
-  value={name}
-  onChange={(e) => setName(e.target.value)}
-/>
-
-<input
-  type="text"
-  value={image}
-  onChange={(e) => setImage(e.target.value)}
-/>
-      </form>
-    </div>
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Toy Name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="Image URL"
+        value={image}
+        onChange={(e) => setImage(e.target.value)}
+      />
+      <button type="submit">Add Toy</button>
+    </form>
   );
-}
-
-function handleSubmit(e) {
-  e.preventDefault();
-
-  const newToy = {
-    name,
-    image,
-    likes: 0
-  };
-
-  fetch("http://localhost:3001/toys", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify(newToy)
-  })
-    .then((res) => res.json())
-    .then((createdToy) => {
-      onAddToy(createdToy);
-    });
 }
 
 export default ToyForm;
